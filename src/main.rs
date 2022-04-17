1548ec7a961fc0d12a6b4f2a64f10c63db0af956
lukasaey <lukagamercoll@gmail.com>
2022-04-17 20:49:23 +0200

    converted everything to use Vector2s

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,35 @@ mod scaler;
 use raylib::prelude::*;
 use std::f32::consts::PI;
 
-const WIDTH: i32 = 1280;
-const HEIGHT: i32 = 720;
-
 const SCROLL_STEP_DOWN: f32 = 1.1;
 const SCROLL_STEP_UP: f32 = 0.9;
 
 fn main() {
-    let (mut rl, thread) = raylib::init().size(WIDTH, HEIGHT).title("vectors").build();
+    let (mut rl, thread) = raylib::init()
+        .size(1280, 720)
+        .resizable()
+        .title("vectors")
+        .build();
 
-    let mut left_holding_x: i32 = 0;
-    let mut left_holding_y: i32 = 0;
+    let mut left_holding_mouse_pos = Vector2::new(0.0, 0.0);
     let mut left_holding = false;
 
-    let mut last_right_x: i32 = 0;
-    let mut last_right_y: i32 = 0;
+    let mut last_mid_mouse_click = Vector2::new(0.0, 0.0);
 
     let mut vectors: Vec<(Vector2, Vector2)> = Vec::new();
 
     let mut scaler = scaler::Scaler {
         scale: 1.0,
-        x_offset: 0.0,
-        y_offset: 0.0,
+        offset: Vector2::new(0.0, 0.0),
     };
 
+    rl.set_target_fps(60);
+
     while !rl.window_should_close() {
-        let mouse_x = rl.get_mouse_x();
-        let mouse_y = rl.get_mouse_y();
+        let mouse_pos = Vector2::new(rl.get_mouse_x() as f32, rl.get_mouse_y() as f32);
 
         if rl.is_mouse_button_down(MouseButton::MOUSE_MIDDLE_BUTTON) {
-            scaler.x_offset -= (mouse_x - last_right_x) as f32 * scaler.scale;
-            scaler.y_offset -= (mouse_y - last_right_y) as f32 * scaler.scale;
+            scaler.offset -= (mouse_pos - last_mid_mouse_click) * scaler.scale;
         } else if rl.is_key_pressed(KeyboardKey::KEY_R) {
             vectors.pop();
         }
@@ -41,27 +39,25 @@ fn main() {
         let scroll = rl.get_mouse_wheel_move();
 
         if scroll != 0.0 {
-            let (x_before_scale, y_before_scale) = scaler.to_world(mouse_x, mouse_y);
+            let before_scale = scaler.to_world(mouse_pos);
 
-            if scroll > 0.0 {
-                let new_scale = scaler.scale * SCROLL_STEP_UP;
-                if new_scale > 0.0 {
-                    scaler.scale = new_scale;
-                }
+            let step = if scroll > 0.0 {
+                SCROLL_STEP_UP
             } else {
-                let new_scale = scaler.scale * SCROLL_STEP_DOWN;
-                if new_scale > 0.0 {
-                    scaler.scale = new_scale;
-                }
+                SCROLL_STEP_DOWN
+            };
+
+            let new_scale = scaler.scale * step;
+            if new_scale > 0.0 {
+                scaler.scale = new_scale;
             }
 
-            let (x_after_scale, y_after_scale) = scaler.to_world(mouse_x, mouse_y);
+            let after_scale = scaler.to_world(mouse_pos);
 
-            scaler.x_offset += x_before_scale - x_after_scale;
-            scaler.y_offset += y_before_scale - y_after_scale;
+            scaler.offset += before_scale - after_scale;
         }
-        last_right_x = mouse_x;
-        last_right_y = mouse_y;
+
+        last_mid_mouse_click = mouse_pos;
 
         let mouse_left_down = rl.is_mouse_button_down(MouseButton::MOUSE_LEFT_BUTTON);
 
@@ -69,35 +65,16 @@ fn main() {
 
         if mouse_left_down && !left_holding {
             // just started holding, setting starting point
-            left_holding_x = mouse_x;
-            left_holding_y = mouse_y;
+            left_holding_mouse_pos = mouse_pos;
             left_holding = true;
         } else if mouse_left_down && left_holding {
             // in the process of holding, draw vector from start to mouse
-            draw_arrow(
-                &mut d,
-                &Vector2::new(
-                    left_holding_x as f32,
-                    left_holding_y as f32,
-                ),
-                &Vector2::new(
-                    mouse_x as f32,
-                    mouse_y as f32,
-                ),
-                7.3,
-                Color::RED,
-            )
+            draw_arrow(&mut d, &left_holding_mouse_pos, &mouse_pos, 7.3, Color::RED)
         } else if !mouse_left_down && left_holding {
             // finished dragging, add the vector to vectors
             vectors.push((
-                scaler.to_world_v(Vector2 {
-                    x: left_holding_x as f32,
-                    y: left_holding_y as f32,
-                }),
-                scaler.to_world_v(Vector2 {
-                    x: mouse_x as f32,
-                    y: mouse_y as f32,
-                }),
+                scaler.to_world(left_holding_mouse_pos),
+                scaler.to_world(mouse_pos),
             ));
             left_holding = false;
         }
@@ -106,8 +83,8 @@ fn main() {
         for vec in &vectors {
             draw_arrow(
                 &mut d,
-                &scaler.to_screen_v(vec.0),
-                &scaler.to_screen_v(vec.1),
+                &scaler.to_screen(vec.0),
+                &scaler.to_screen(vec.1),
                 7.3,
                 Color::BLUE,
             );
@@ -115,7 +92,7 @@ fn main() {
     }
 }
 
-fn draw_arrow(d: &mut RaylibDrawHandle<'_>, p0: &Vector2, p1: &Vector2, radius: f32, color: Color) {
+fn draw_arrow(d: &mut RaylibDrawHandle, p0: &Vector2, p1: &Vector2, radius: f32, color: Color) {
     d.draw_line_v(p0, p1, color);
 
     let dx = p1.x - p0.x;
--- src/scaler.rs
+++ src/scaler.rs
@@ -2,32 +2,15 @@ use raylib::prelude::*;
 
 pub struct Scaler {
     pub scale: f32,
-    pub x_offset: f32,
-    pub y_offset: f32,
+    pub offset: Vector2,
 }
 
 impl Scaler {
-    pub fn _to_screen(&self, x: f32, y: f32) -> (i32, i32) {
-        (
-            ((x + self.x_offset) / self.scale) as i32,
-            ((y + self.y_offset) / self.scale) as i32,
-        )
+    pub fn to_screen(&self, v: Vector2) -> Vector2 {
+        (v - self.offset) / self.scale
     }
 
-    pub fn to_world(&self, x: i32, y: i32) -> (f32, f32) {
-        (
-            (x as f32 * self.scale) - self.x_offset,
-            (y as f32 * self.scale) - self.y_offset,
-        )
-    }
-
-    pub fn to_screen_v(&self, v: Vector2) -> Vector2 {
-        let offset = Vector2::new(self.x_offset, self.y_offset);
-        (v - offset) / self.scale
-    }
-
-    pub fn to_world_v(&self, v: Vector2) -> Vector2 {
-        let offset = Vector2::new(self.x_offset, self.y_offset);
-        (v * self.scale) + offset
+    pub fn to_world(&self, v: Vector2) -> Vector2 {
+        (v * self.scale) + self.offset
     }
 }
